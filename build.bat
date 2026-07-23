328b8e10fd7fd1f80f109563401268a85645bc0d
IndexZero <lingup-cn@qq.com>
2026-07-23 19:34:35 +0800

    update

--- build.bat
+++
@@ -1,3 +0,0 @@
-scons platform=windows target=template_debug
-scons platform=windows target=template_release
-xcopy /Y /I "project\addons\beat_track\bin\*" "E:\Projects\aud-editor\addons\beat_track\bin\"
